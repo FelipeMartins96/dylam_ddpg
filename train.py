7dcd1e9cea7b66e0bb29143bef711f184d3fbaf0
Felipe Martins <fbm2@cin.ufpe.br>
2022-08-16 17:47:12 -0300

    testing experiments

--- train.py
+++ train.py
@@ -4,10 +4,10 @@ from runners import runner
 
 @hydra.main(version_base=None, config_path="configs/", config_name="config")
 def main(cfg):
-    if cfg.exp_name is None:
-        cfg.exp_name = os.path.basename(__file__).rstrip(".py")
+    if cfg.experiment.exp_name is None:
+        cfg.experiment.exp_name = os.path.basename(__file__).rstrip(".py")
     
-    return runner(cfg)
+    return runner(cfg.experiment)
 
 if __name__ == "__main__":
     main()
