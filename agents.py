3e9987ffea5c3ca20361b47df80f5e27ac33813c
Felipe Martins <fbm2@cin.ufpe.br>
2022-08-19 15:20:02 -0300

    change agents code order

--- agents.py
+++ agents.py
@@ -24,6 +24,121 @@ class StratLastRewards:
     def mean(self):
         return self.rewards.mean(0)
 
+class AgentDDPG:
+    def __init__(
+            self, 
+            envs, 
+            device,
+            batch_size,
+            buffer_size,
+            exploration_noise,
+            gamma,
+            learning_rate_q,
+            learning_rate_actor,
+            learning_starts,
+            policy_frequency,
+            tau,
+        ):
+        self.actor = Actor(envs).to(device)
+        self.qf1 = QNetwork(envs).to(device)
+        self.qf1_target = QNetwork(envs).to(device)
+        self.target_actor = Actor(envs).to(device)
+        self.target_actor.load_state_dict(self.actor.state_dict())
+        self.qf1_target.load_state_dict(self.qf1.state_dict())
+        self.q_optimizer = optim.Adam(list(self.qf1.parameters()), lr=learning_rate_q)
+        self.actor_optimizer = optim.Adam(list(self.actor.parameters()), lr=learning_rate_actor)
+
+        self.num_rewards = 1
+
+        envs.single_observation_space.dtype = np.float32
+        self.rb = StratReplayBuffer(
+            int(buffer_size),
+            envs.single_observation_space,
+            envs.single_action_space,
+            self.num_rewards,
+            device,
+            handle_timeout_termination=False,
+        )
+
+        self.device = device
+        self.envs = envs
+        self.exploration_noise = exploration_noise
+        self.learning_starts = int(learning_starts)
+        self.batch_size = int(batch_size)
+        self.gamma = gamma
+        self.tau = tau
+        self.policy_frequency = int(policy_frequency)
+
+    def sample_actions(self, obs):
+        with torch.no_grad():
+            if self.rb.size() != 0 and self.rb.size() < self.learning_starts:
+                actions = self.actor.action_bias.expand(self.envs.num_envs,-1).clone()
+            else:
+                actions = self.actor(torch.Tensor(obs).to(self.device))
+            actions += torch.normal(self.actor.action_bias.expand(self.envs.num_envs,-1), self.actor.action_scale * self.exploration_noise)
+            actions = actions.cpu().numpy().clip(self.envs.single_action_space.low, self.envs.single_action_space.high)
+        return actions
+
+    def observe(self, obs, _obs, actions, rws, dones, infos, actor_key):
+        for idx, d in enumerate(dones):
+            s = slice(idx,idx+1)
+            self.rb.add(
+                obs[s],
+                _obs[s],
+                actions[s],
+                rws[s].reshape(1,-1),
+                dones[s],
+                infos[s]
+            )
+    
+    def update(self, global_step):
+        if self.rb.size() < self.learning_starts or self.rb.size() == 0:
+            return None
+
+        info = {}
+        data = self.rb.sample(self.batch_size)
+
+        with torch.no_grad():
+            next_state_actions = self.target_actor(data.next_observations)
+            qf1_next_targets = self.qf1_target(data.next_observations, next_state_actions)
+            next_q_values = data.rewards + (1 - data.dones.expand(-1, self.num_rewards)) * self.gamma * qf1_next_targets
+
+        qf1_a_values = self.qf1(data.observations, data.actions)
+        qf1_losses = torch.nn.MSELoss(reduction='none')(qf1_a_values, next_q_values).mean(0)
+        qf1_loss = torch.sum(qf1_losses)
+
+        # optimize the model
+        self.q_optimizer.zero_grad()
+        qf1_loss.backward()
+        self.q_optimizer.step()
+
+        info['losses/qf1_loss'] = qf1_loss.item()
+        with torch.no_grad():
+            qf1_a_values_mean = qf1_a_values.mean(0)
+        info[f'rw_total/qf1_a_value'] = qf1_a_values_mean[0].item()
+
+        if global_step % self.policy_frequency == 0:
+            qf1s = self.qf1(data.observations, self.actor(data.observations))
+            actor_loss = -qf1s.mean()
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+
+            # update the target network
+            for param, target_param in zip(self.actor.parameters(), self.target_actor.parameters()):
+                target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+            for param, target_param in zip(self.qf1.parameters(), self.qf1_target.parameters()):
+                target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+            info.update({
+                "losses/actor_loss": actor_loss.item(),
+            })
+
+        return info
+
+    def save_actor(self, path):
+        torch.save(self.actor.state_dict(), path)
+
 class AgentDylamDDPG:
     def __init__(
             self, 
@@ -148,122 +263,6 @@ class AgentDylamDDPG:
         if global_step % self.policy_frequency == 0:
             qf1s = self.qf1(data.observations, self.actor(data.observations))
             actor_loss = -(qf1s * lambdas).sum(1).mean()
-            self.actor_optimizer.zero_grad()
-            actor_loss.backward()
-            self.actor_optimizer.step()
-
-            # update the target network
-            for param, target_param in zip(self.actor.parameters(), self.target_actor.parameters()):
-                target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
-            for param, target_param in zip(self.qf1.parameters(), self.qf1_target.parameters()):
-                target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
-
-            info.update({
-                "losses/actor_loss": actor_loss.item(),
-            })
-
-        return info
-
-    def save_actor(self, path):
-        torch.save(self.actor.state_dict(), path)
-
-
-class AgentDDPG:
-    def __init__(
-            self, 
-            envs, 
-            device,
-            batch_size,
-            buffer_size,
-            exploration_noise,
-            gamma,
-            learning_rate_q,
-            learning_rate_actor,
-            learning_starts,
-            policy_frequency,
-            tau,
-        ):
-        self.actor = Actor(envs).to(device)
-        self.qf1 = QNetwork(envs).to(device)
-        self.qf1_target = QNetwork(envs).to(device)
-        self.target_actor = Actor(envs).to(device)
-        self.target_actor.load_state_dict(self.actor.state_dict())
-        self.qf1_target.load_state_dict(self.qf1.state_dict())
-        self.q_optimizer = optim.Adam(list(self.qf1.parameters()), lr=learning_rate_q)
-        self.actor_optimizer = optim.Adam(list(self.actor.parameters()), lr=learning_rate_actor)
-
-        self.num_rewards = 1
-
-        envs.single_observation_space.dtype = np.float32
-        self.rb = StratReplayBuffer(
-            int(buffer_size),
-            envs.single_observation_space,
-            envs.single_action_space,
-            self.num_rewards,
-            device,
-            handle_timeout_termination=False,
-        )
-
-        self.device = device
-        self.envs = envs
-        self.exploration_noise = exploration_noise
-        self.learning_starts = int(learning_starts)
-        self.batch_size = int(batch_size)
-        self.gamma = gamma
-        self.tau = tau
-        self.policy_frequency = int(policy_frequency)
-
-    def sample_actions(self, obs):
-        with torch.no_grad():
-            if self.rb.size() != 0 and self.rb.size() < self.learning_starts:
-                actions = self.actor.action_bias.expand(self.envs.num_envs,-1).clone()
-            else:
-                actions = self.actor(torch.Tensor(obs).to(self.device))
-            actions += torch.normal(self.actor.action_bias.expand(self.envs.num_envs,-1), self.actor.action_scale * self.exploration_noise)
-            actions = actions.cpu().numpy().clip(self.envs.single_action_space.low, self.envs.single_action_space.high)
-        return actions
-
-    def observe(self, obs, _obs, actions, rws, dones, infos, actor_key):
-        for idx, d in enumerate(dones):
-            s = slice(idx,idx+1)
-            self.rb.add(
-                obs[s],
-                _obs[s],
-                actions[s],
-                rws[s].reshape(1,-1),
-                dones[s],
-                infos[s]
-            )
-    
-    def update(self, global_step):
-        if self.rb.size() < self.learning_starts or self.rb.size() == 0:
-            return None
-
-        info = {}
-        data = self.rb.sample(self.batch_size)
-
-        with torch.no_grad():
-            next_state_actions = self.target_actor(data.next_observations)
-            qf1_next_targets = self.qf1_target(data.next_observations, next_state_actions)
-            next_q_values = data.rewards + (1 - data.dones.expand(-1, self.num_rewards)) * self.gamma * qf1_next_targets
-
-        qf1_a_values = self.qf1(data.observations, data.actions)
-        qf1_losses = torch.nn.MSELoss(reduction='none')(qf1_a_values, next_q_values).mean(0)
-        qf1_loss = torch.sum(qf1_losses)
-
-        # optimize the model
-        self.q_optimizer.zero_grad()
-        qf1_loss.backward()
-        self.q_optimizer.step()
-
-        info['losses/qf1_loss'] = qf1_loss.item()
-        with torch.no_grad():
-            qf1_a_values_mean = qf1_a_values.mean(0)
-        info[f'rw_total/qf1_a_value'] = qf1_a_values_mean[0].item()
-
-        if global_step % self.policy_frequency == 0:
-            qf1s = self.qf1(data.observations, self.actor(data.observations))
-            actor_loss = -qf1s.mean()
             self.actor_optimizer.zero_grad()
             actor_loss.backward()
             self.actor_optimizer.step()
