f8293915fb260a3e0a48ab7824a298c680651923
Felipe Martins <felipemartins96@gmail.com>
2022-08-24 20:07:29 -0300

    06/24 experiments

--- envs/__init__.py
+++ envs/__init__.py
@@ -20,3 +20,10 @@ register(
     kwargs={'n_robots_blue': 2, 'n_robots_yellow': 1},
     max_episode_steps=1200,
 )
+
+register(
+    id="vssStrat1v2-v0",
+    entry_point="envs.vss_strat:VSSStratEnv",
+    kwargs={'n_robots_blue': 1, 'n_robots_yellow': 2},
+    max_episode_steps=1200,
+)
